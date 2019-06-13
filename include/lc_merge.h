039edf8dae3be40f3da0ffe338e4726f480eae2c
Moloy <moloy.bitspilani@gmail.com>
2019-06-13 15:40:17 +0530

    moved all the includes to .h file in include folder

---
+++ include/lc_merge.h
@@ -0,0 +1,22 @@
+#ifndef LC_MERGE_H_
+#define LC_MERGE_H_
+
+#include <math.h>
+#include <iostream>
+#include <fstream>
+
+#include <ros/ros.h>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/common/transforms.h>
+
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+
+#endif
--- lc_merge.cpp
+++ lc_merge.cpp
@@ -1,20 +1,4 @@
-#include <ros/ros.h>
-#include <math.h>
-
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl_ros/point_cloud.h>
-#include <pcl/common/transforms.h>
-
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
-
-#include <iostream>
-#include <fstream>
+#include <lc_merge.h>
 
 using namespace std;
 
